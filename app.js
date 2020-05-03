b22022eef064ab82e58986e3ad54b70c3d1d6c7b
rishi28931 <32589481+rishi28931@users.noreply.github.com>
2020-05-03 21:11:34 +0530

    upto starting of handlebars

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const http = require('http');
 const bodyParser = require('body-parser');
 const express = require('express');
+const expresshbs = require("express-handlebars");
 const app = express();
+app.engine('hb', expresshbs())
+app.set('view-engine', 'hb');
+app.set('views', 'views');
 const adminroutes = require("./routes/admin.js");
 
 
